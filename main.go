0a9a08b1e1f0feb3d9f75b1727a5ffad0fb674ce
BlackDandel10n <blackdandel10n@proton.me>
2025-07-18 17:47:01 +0330

    Add basic logging

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"log"
+	"os"
 )
 
 
@@ -12,6 +14,17 @@ func handler(writer http.ResponseWriter, req *http.Request) {
 
 
 func main() {
+	// Logger
+	logger := log.New(os.Stdout, "[+]: ", log.LstdFlags)
+	
+	// Initialize server
 	http.HandleFunc("GET /", handler)
-	http.ListenAndServe(":8080", nil)
+	logger.Println("Starting server...")
+	
+	// Server logic
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		logger.Fatal("ListenAndServe ", err)
+	}
+
 }
